830af0af36b304c914d41a3bf9ccdd00ea29ef8b
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-17 10:08:06 -0300

    chore: update shareCode to IB4IK7G2J55

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=H4ICCJ86231&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=IB4IK7G2J55&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
