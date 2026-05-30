d94fb7075ead8ae9f533635ad778c2d3d4d3086f
JCPrime01 <vanzokiller@gmail.com>
2026-05-30 09:27:02 -0300

    chore: shareCode EX4ENEVAXLO -> YWFFGUFRR3S (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=EX4ENEVAXLO&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=YWFFGUFRR3S&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
