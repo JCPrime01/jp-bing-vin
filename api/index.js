09aae5b205cab9cbabf8a88a23ebd61e51362270
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 09:59:37 -0300

    chore: shareCode LF56FNDMKJG -> H8RMSAMUZHS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=LF56FNDMKJG&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=H8RMSAMUZHS&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
