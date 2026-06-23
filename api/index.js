b42b5e57bfa65800f21fadd72f312b9b6b3504f1
JCPrime01 <vanzokiller@gmail.com>
2026-06-22 22:24:49 -0300

    chore: shareCode R5N4EHPEBLJ -> AD96C4AOQE4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=R5N4EHPEBLJ&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=AD96C4AOQE4&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
