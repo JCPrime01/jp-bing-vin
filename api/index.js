32e7f354db50f13194e68db2683a211636f4cf78
JCPrime01 <vanzokiller@gmail.com>
2026-06-29 09:39:21 -0300

    chore: shareCode EGDFUGHR1MT -> OFU1VE0JODS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=EGDFUGHR1MT&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=OFU1VE0JODS&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
